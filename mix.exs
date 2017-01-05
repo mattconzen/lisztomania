e250ca8ff38f90d62a1c701fcc80b5ca866e3f9d
Matt Conzen <mattconzen@gmail.com>
2017-01-04 23:29:18 -0600

    Added spotify_ex dependency, updated all deps

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Lisztomania.Mixfile do
   def application do
     [mod: {Lisztomania, []},
      applications: [:phoenix, :phoenix_pubsub, :phoenix_html, :cowboy, :logger, :gettext,
-                    :phoenix_ecto, :postgrex]]
+                    :phoenix_ecto, :postgrex, :spotify_ex]]
   end
 
   # Specifies which paths to compile per environment.
@@ -30,14 +30,17 @@ defmodule Lisztomania.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
+    [
+     {:phoenix, "~> 1.2.1"},
      {:phoenix_pubsub, "~> 1.0"},
      {:phoenix_ecto, "~> 3.0"},
      {:postgrex, ">= 0.0.0"},
      {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+     {:cowboy, "~> 1.0"},
+     {:spotify_ex, "~> 2.0.4"}
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.

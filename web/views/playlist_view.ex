c1e87e8c169dad626ff63891438b96b827971ead
Matt Conzen <mattconzen@gmail.com>
2017-02-25 17:45:06 -0600

    Remove unused render functions

--- web/views/playlist_view.ex
+++ web/views/playlist_view.ex
@@ -1,20 +1,10 @@
 defmodule Lisztomania.PlaylistView do
   use Lisztomania.Web, :view
 
-  def render("index.json", %{playlists: playlists}) do
-    %{data: render_many(playlists, Lisztomania.PlaylistView, "playlist.json")}
-  end
-
   def render("show.json", %{playlists: playlists}) do
     %{
       playlists: Enum.map(playlists, &playlist_json/1)
     }
-  end
-
-  def render("playlist.json", %{playlist: playlist}) do
-    %{id: playlist.id,
-      user_id: playlist.user_id,
-      playlist_id: playlist.playlist_id}
   end
 
   def playlist_json(playlist) do

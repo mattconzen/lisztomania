a06c71be8cd64c7b7dc159ed407adeda77e34502
Matt Conzen <mattconzen@gmail.com>
2017-02-26 21:26:49 -0600

    Creating playlists now supported via POST to /users/{user_id}/playlists

--- web/controllers/playlist_controller.ex
+++ web/controllers/playlist_controller.ex
@@ -3,8 +3,8 @@ defmodule Lisztomania.PlaylistController do
   use Lisztomania.Web, :controller
   plug Lisztomania.Plugs.Auth
 
-  def index(conn, %{"user_id" => user}) do
-    case Spotify.Playlist.get_users_playlists(conn, user) do 
+  def index(conn, %{"user_id" => user_id}) do
+    case Spotify.Playlist.get_users_playlists(conn, user_id) do 
       { :ok, response } -> 
         render(conn, "show.json", playlists: response.items)
       { :error, message } ->
@@ -12,8 +12,14 @@ defmodule Lisztomania.PlaylistController do
     end
   end
 
-  def create(conn, _params) do
-    render conn, "errors.json", message: "oops"
+  def create(conn, %{"user_id" => user_id, "name" => name}) do
+    IEx.pry
+    case Spotify.Playlist.create_playlist(conn, user_id, Poison.encode!(%{name: name})) do
+      { :ok, new_playlist } ->
+        render(conn, "show.json", playlist: new_playlist)
+      { :error, message } ->
+        render(conn, "errors.json", message: "oops")
+    end
   end
 
   def show(conn, _params) do

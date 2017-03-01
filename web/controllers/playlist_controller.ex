ad70c0a0d38b31a3c5083f8d598f5d0132e850d9
Matt Conzen <mattconzen@gmail.com>
2017-02-28 23:31:44 -0600

    WIP commit

--- web/controllers/playlist_controller.ex
+++ web/controllers/playlist_controller.ex
@@ -13,7 +13,6 @@ defmodule Lisztomania.PlaylistController do
   end
 
   def create(conn, %{"user_id" => user_id, "name" => name}) do
-    IEx.pry
     case Spotify.Playlist.create_playlist(conn, user_id, Poison.encode!(%{name: name})) do
       { :ok, new_playlist } ->
         render(conn, "show.json", playlist: new_playlist)
@@ -26,9 +25,28 @@ defmodule Lisztomania.PlaylistController do
     render conn, "errors.json", message: "oops"    
   end
 
-  def update(conn, _params) do
-    render conn, "errors.json", message: "oops"
+  def update(conn, %{"user_id" => user_id, "id" => playlist_id, "album_id" => album_id}) do
+    IEx.pry
+    body = Poison.encode!(%{ uris: get_track_ids_for_album(conn, album_id)})
+    case Spotify.Playlist.add_tracks(conn, user_id, playlist_id, body, []) do
+      {:ok, response} ->
+        render(conn, "show.json", playlist: response)
+      {:error, message} ->
+        render(conn, "errors.json", message: message)
+    end
   end
+
+# body = Poison.encode!(%{ uris: [ "spotify:track:755MBpLqJqCO87PkoyBBQC", "spotify:track:1hsWu8gT2We6OzjhYGAged" ]})
+
+  def get_track_ids_for_album(conn, album_id) do
+    IEx.pry
+    case Spotify.Album.get_album_tracks(conn, album_id) do
+      {:ok, response} ->
+        Enum.into(response.items, [], fn t -> t.uri end)
+      {:error, message} ->
+        render(conn, "errors.json", message: message)
+    end
+  end 
 
   def delete(conn, _params) do
     render conn, "errors.json", message: "oops"

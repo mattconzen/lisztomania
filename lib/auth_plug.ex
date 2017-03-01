072eb5a1b367f29cccee349e344ddc2f30d09ff7
Matt Conzen <mattconzen@gmail.com>
2017-03-01 00:09:29 -0600

    Credo suggestion:  shouldn't have an else block

--- lib/auth_plug.ex
+++ lib/auth_plug.ex
@@ -1,13 +1,13 @@
-defmodule Lisztomania.Plugs.Auth do 
+defmodule Lisztomania.Plugs.Auth do
   import Plug.Conn
 
   def init(default), do: default
 
-  def call(conn, _default) do 
-    unless Spotify.Authentication.authenticated?(conn) do
-      Phoenix.Controller.redirect conn, external: Spotify.Authorization.url
-    else
+  def call(conn, _default) do
+    if Spotify.Authentication.authenticated?(conn) do
       conn
+    else
+      Phoenix.Controller.redirect conn, external: Spotify.Authorization.url
     end
   end
 end
